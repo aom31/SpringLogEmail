dd6b68028c7e6adee7680d78c6abbaf40fe68498
aom31 <97218117+aom31@users.noreply.github.com>
2022-06-25 18:23:11 +0700

    create

---
+++ src/main/java/com/example/securityemailverify/SecurityemailverifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityemailverify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityemailverifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityemailverifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/securityemailverify/SecurityemailverifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityemailverify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityemailverifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
