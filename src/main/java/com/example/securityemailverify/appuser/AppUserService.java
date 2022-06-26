1b9ef63c2032f8edbb3e1e05ba401da845d9fd92
aom31 <97218117+aom31@users.noreply.github.com>
2022-06-26 16:36:59 +0700

    confirmation token

--- src/main/java/com/example/securityemailverify/appuser/AppUserService.java
+++ src/main/java/com/example/securityemailverify/appuser/AppUserService.java
@@ -1,5 +1,7 @@
 package com.example.securityemailverify.appuser;
 
+import com.example.securityemailverify.registration.token.ConfirmationToken;
+import com.example.securityemailverify.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,6 +9,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -17,6 +22,9 @@ public class AppUserService implements UserDetailsService {
     private final AppUserRepository appUserRepository;
 
     private  final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+    private final ConfirmationTokenService confirmationTokenService;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
@@ -31,8 +39,15 @@ public class AppUserService implements UserDetailsService {
         String encoderPassword =  bCryptPasswordEncoder.encode(appUser.getPassword());
         appUser.setPassword(encoderPassword);
         appUserRepository.save(appUser);
-        //send confirtmation token
 
-        return "it work";
+        String token = UUID.randomUUID().toString();
+        //send confirtmation token
+        ConfirmationToken confirmationToken = new ConfirmationToken(token,
+                                                LocalDateTime.now(), LocalDateTime.now().plusMinutes(15)
+                                                ,appUser);
+        confirmationTokenService.saveConfirmationToken(confirmationToken);
+
+        //send email
+        return token;
     }
 }
---
+++ src/main/java/com/example/securityemailverify/registration/token/ConfirmationTokenService.java
@@ -0,0 +1,14 @@
+package com.example.securityemailverify.registration.token;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ConfirmationTokenService {
+        private final ConfirmationTokenRepository confirmationTokenRepository;
+
+        public void saveConfirmationToken(ConfirmationToken confirmationToken){
+            confirmationTokenRepository.save(confirmationToken);
+        }
+}
